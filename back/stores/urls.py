9d5b9d8e204513654f96d53946eff8848ceecf84
ligaya-milangela <125577516+ligaya-milangela@users.noreply.github.com>
2025-03-29 03:39:13 +0800

    Delete back directory

--- back/stores/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import SignupView, LoginView
-
-urlpatterns = [
-    path('signup/', SignupView.as_view(), name='signup'),  # Add trailing slash
-    path('login/', LoginView.as_view(), name='login'),
-]
