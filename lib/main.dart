f2cc1a6f79fd91e3a8d42c53ceb9baa1f3298f7e
Lurd Synell B. Najarila <lnajarila@gbox.adnu.edu.ph>
2025-04-14 21:18:10 +0800

    Fix duplicate initialization of [DEFAULT] Firebase app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'theme/app_theme.dart';
@@ -6,18 +7,22 @@ import 'screens/login_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-  options: const FirebaseOptions(
-    apiKey: 'AIzaSyAY-2R0EX1vY1T5UTSJSrVLV-whQSmjU1w',
-    appId: '1:342791532648:web:85714f4e0b1c238cb6cbdd',
-    messagingSenderId: '342791532648',
-    projectId: 'tindahanap',
-    authDomain: 'tindahanap.firebaseapp.com',
-    storageBucket: 'tindahanap.appspot.com',
-    measurementId: 'G-DST5DB9KXB',
-  ),
-);
-
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyAY-2R0EX1vY1T5UTSJSrVLV-whQSmjU1w',
+        appId: '1:342791532648:web:85714f4e0b1c238cb6cbdd',
+        messagingSenderId: '342791532648',
+        projectId: 'tindahanap',
+        authDomain: 'tindahanap.firebaseapp.com',
+        storageBucket: 'tindahanap.appspot.com',
+        measurementId: 'G-DST5DB9KXB',
+      ),
+    );
+  } else {
+    // Android: Use google-services.json
+    await Firebase.initializeApp();
+  }
 
   runApp(const TindahanapApp());
 }
