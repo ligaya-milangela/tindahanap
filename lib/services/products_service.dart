e2b93dd5d45e6db68994d7cbc6e3ac1c6fb691a5
Lurd Synell B. Najarila <lnajarila@gbox.adnu.edu.ph>
2025-05-18 17:18:12 +0800

    Add missing Convenience category in product sorting

--- lib/services/products_service.dart
+++ lib/services/products_service.dart
@@ -2,7 +2,7 @@ import '../api/products.dart';
 import '../models/product.dart';
 
 Future<List<Product>> getStoreProducts(String storeId) async {
-  const List<String> categories = ['Food', 'Drinks', 'Hygiene', 'Medicine', 'Household'];
+  const List<String> categories = ['Food', 'Drinks', 'Hygiene', 'Medicine', 'Household', 'Convenience'];
   List<Product> products = await getProducts(storeId);
 
   // Sort by category: Food, Drinks, Hygiene, Medicine, Household
